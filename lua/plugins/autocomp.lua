6c4e0e74d88a8ed0772275295a0a70d37878469e
Vikhyath V Shetty <vikhyath.v.shetty@gmail.com>
2026-02-03 10:46:49 +0530

    copilotChat.lua:added new keymap

--- lua/plugins/autocomp.lua
+++ lua/plugins/autocomp.lua
@@ -1,20 +1,19 @@
-
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  config = function()
-    local cmp = require("cmp")
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	config = function()
+		local cmp = require("cmp")
 
-    cmp.setup({
-      mapping = cmp.mapping.preset.insert({
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-      }),
-      sources = {
-        { name = "nvim_lsp" },
-      },
-    })
-  end,
+		cmp.setup({
+			mapping = cmp.mapping.preset.insert({
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+			}),
+			sources = {
+				{ name = "nvim_lsp" },
+			},
+		})
+	end,
 }
--- lua/plugins/copilotChat.lua
+++ lua/plugins/copilotChat.lua
@@ -1,12 +1,14 @@
 return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "nvim-lua/plenary.nvim", branch = "master" },
-    },
-    build = "make tiktoken",
-    opts = {
-      -- See Configuration section for options
-    },
-  },
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim", branch = "master" },
+			{ "github/copilot.vim" },
+		},
+		build = "make tiktoken",
+		opts = {},
+		config = function()
+			vim.keymap.set("n", "<leader>cc", ":CopilotChat<CR>", { desc = "Open Copilot Chat" })
+		end,
+	},
 }
