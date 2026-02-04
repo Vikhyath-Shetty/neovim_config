41fbd0b42ae21fc7858e97f9f763b662781d7af4
Vikhyath V Shetty <vikhyath.v.shetty@gmail.com>
2026-02-04 12:35:55 +0530

    modified: copilot.lua

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,20 @@
 return {
-  "github/copilot.vim",
-  lazy = false,
+	"github/copilot.vim",
+	lazy = false,
+	init = function()
+		vim.g.copilot_enabled = 0
+	end,
+	config = function()
+		vim.keymap.set("n", "<leader>cp", function()
+			if vim.g.copilot_enabled == 1 then
+				vim.cmd("Copilot disable")
+				vim.g.copilot_enabled = 0
+				print("Copilot disabled")
+			else
+				vim.cmd("Copilot enable")
+				vim.g.copilot_enabled = 1
+				print("Copilot enabled")
+			end
+		end, { desc = "Toggle GitHub Copilot" })
+	end,
 }
